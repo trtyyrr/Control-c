33a8150151b3f8618ed9d6136bab4e75d83bed33
trtyyrr <m17752427945@163.com>
2026-01-14 21:32:54 +0800

    创建 main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[]) {
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    
+    // 加载 Main 模块中的 Main.qml
+    const QUrl url(u"qrc:/qt/qml/Main/Main.qml"_qs);
+    
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+
+    engine.load(url);
+
+    return app.exec();
+}
